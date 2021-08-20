673f912aadc73459536a14fa3fa0d342e8664659
Sean <seaninho10@gmail.com>
2021-08-20 22:20:20 +0300

    Refactor: Syntax change

--- api/routes/faculties.js
+++ api/routes/faculties.js
@@ -2,12 +2,12 @@ const Faculties = require('../models/faculty');
 const databaseHandler = require('../middleware/graphDBHandler');
 const writeResponse = require('../helpers/response').writeResponse;
 
-const getFacultyById = function (req, res) {
+function getFacultyById(req, res) {
     Faculties.getFacultyById(databaseHandler.getSession(req), req.params.id)
         .then(response => writeResponse(res, response));
 }
 
-const listAllFaculties = function (req, res) {
+function listAllFaculties(req, res) {
     Faculties.getAllFaculties(databaseHandler.getSession(req))
         .then(response => writeResponse(res, response));
 }
