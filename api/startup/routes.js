8e2e36f8bd16f3474c62b0c048f061c68f8d8d11
Sean <seaninho10@gmail.com>
2021-08-20 22:22:47 +0300

    Functionality: Added list all entities routes

--- api/startup/routes.js
+++ api/startup/routes.js
@@ -13,11 +13,17 @@ function route(app) {
     app.get("/database/delete", dbHandler.deleteDatabase);
     app.get("/faculty", faculties.listAllFaculties);    
     app.get("/faculty/:id", faculties.getFacultyById);
+    app.get("/lab", labs.listAllLabs);
     app.get("/lab/:id", labs.getLabById);
+    app.get("/researcher", researchers.listAllResearchers);
     app.get("/researcher/:id", researchers.getResearcherById);
+    app.get("/researchArea", researchAreas.listAllResearchAreas);
     app.get("/researchArea/:id", researchAreas.getResearchAreaById);
+    app.get("/research", researches.listAllResearches);
     app.get("/research/:id", researches.getResearchById);
+    app.get("/researchSetup", researchSetups.listAllResearchSetups);
     app.get("/researchSetup/:id", researchSetups.getResearchSetupById);
+    app.get("/product", products.listAllProducts);
     app.get("/product/:id", products.getProductById);
 }
 
